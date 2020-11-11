00bd197012620232e45e9cbf12bb02423f71ffd3
a.romza <a.romza@itfbgroup.ru>
2020-11-11 23:04:40 +0300

    (feature): select default intervals

--- src/components/ScheduleDatePicker.tsx
+++ src/components/ScheduleDatePicker.tsx
@@ -1,28 +1,30 @@
 import React from 'react';
 import { DatePicker } from 'antd';
 import moment, { Moment } from 'moment';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { RangeValue } from 'rc-picker/lib/interface';
 import { setStartDate, setEndDate } from './params.slice';
 import locale from 'antd/es/date-picker/locale/ru_RU';
+import { RootState } from '../store/store';
 
 const { RangePicker } = DatePicker;
 
 function disabledDate(current: Moment) {
-    return current && current <= moment().startOf('day');
+    return current && current <= moment().startOf('day').subtract(1, 'day');
   }
 
 export default function ScheduleDatePicker() {
     const dispatch = useDispatch();
+    const [startDate, endDate] = useSelector((state: RootState) => [
+        moment(state.params.startDate).startOf('day'),
+        moment(state.params.endDate).endOf('day')]);
     const onChange = (values: RangeValue<Moment>, formatString: [string, string]) => {
-        console.log(formatString);
-        console.log(values);
         if (values?.every(v => !!v)) {
             dispatch(setStartDate(formatString[0]));
             dispatch(setEndDate(formatString[1]));
         }
     }
     return (
-        <RangePicker locale={locale} onChange={onChange} defaultValue={[moment(), moment().add(1, 'week')]} disabledDate={disabledDate}/>
+        <RangePicker locale={locale} value={[startDate, endDate]} onChange={onChange} disabledDate={disabledDate}/>
     );
 }
--- src/components/ScheduleHeader.tsx
+++ src/components/ScheduleHeader.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import moment from 'moment';
 import { Layout, PageHeader, Typography, Radio } from 'antd';
+import { setStartDate, setEndDate } from './params.slice';
+import { DEFAULT_DATE_FORMAT } from '../constants';
 import './ScheduleHeader.scss';
+import { useDispatch } from 'react-redux';
 
 const { Header } = Layout;
 
 export default function ScheduleHeader() {
+    const dispatch = useDispatch();
+    const setDates = (days: number) => {
+        dispatch(setStartDate(moment().startOf('day').format(DEFAULT_DATE_FORMAT)));
+        dispatch(setEndDate(moment().startOf('day').add(days, 'day').format(DEFAULT_DATE_FORMAT)));
+    };
     return (
         <Header className='sheduling-header'>
             <PageHeader
@@ -12,9 +21,9 @@ export default function ScheduleHeader() {
                 title={<Typography.Title level={4}>Stuff schedue</Typography.Title>}
                 extra={[
                     <Radio.Group defaultValue='1' key='1'>
-                        <Radio.Button value='1'>1 day</Radio.Button>
-                        <Radio.Button value='2'>2 days</Radio.Button>
-                        <Radio.Button value='3'>Week</Radio.Button>
+                        <Radio.Button value='1' onClick={() => setDates(0)}>1 day</Radio.Button>
+                        <Radio.Button value='2' onClick={() => setDates(2)}>2 days</Radio.Button>
+                        <Radio.Button value='3' onClick={() => setDates(7)}>Week</Radio.Button>
                     </Radio.Group>
                 ]}
             />
